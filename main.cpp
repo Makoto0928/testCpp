7e53922877318f98988ba519c71f0a1757941dab
Tsuruoka Makoto <makotty0928@gmail.com>
2019-12-29 09:26:54 +0900

    first commit C++ Project

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "C++ Project Start!" << endl;
+    cout << "My Name Is Makoto Tsuruoka! I Start C++ Project!" << endl;
+    return 0;
+}
